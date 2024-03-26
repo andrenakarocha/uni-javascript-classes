cf47335d3fb7897e92f1be9c34f0575d962fe1d3
André Rocha <andrenakarocha@hotmail.com>
2024-03-26 09:38:00 -0300

    Estruturas de Repetição~

---
+++ Exemplo1/media.js
@@ -0,0 +1,21 @@
+function media() {
+    let nome = window.prompt('Qual o nome do aluno?');
+    let n1 = Number(window.prompt(`Qual foi a primeira nota de ${nome}?`));
+    let n2 = Number(window.prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`));
+    med = (n1 + n2) / 2;
+
+
+let msg;
+if (med >= 6) {
+    msg = 'Meus parabéns!';
+}
+else {
+    msg = 'Estude um pouco mais!';
+};
+
+let res = document.getElementById('texto');
+res.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark>.</p>`;
+res.innerHTML += `<p>As notas obtidas foram <mark>${n1} e ${n2}</mark>.</p>`;
+res.innerHTML += `<p> média final será <mark>${med}</mark>.</p>`;
+res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red;'>${msg}</strong></p>`;
+}
---
+++ Repetição.js
@@ -0,0 +1,59 @@
+/*var jogador1 = 1;
+var jogador2 = 0;
+var placar;
+
+//Diferente If Else
+jogador1 > 0 ? console.log('marcou ponto') : console.log('nao marcou ponto');
+
+//Switch, Case e Default
+
+switch (placar) {
+    case placar = jogador1 > jogador2:
+        console.log('Jogador 1 ganhou');
+        break; 
+    case placar = jogador2 > jogador1:
+        console.log('Jogador 2 ganhou')
+        break;
+    default:
+        console.log('Ninguem ganhou')
+}
+
+//Repetição - For
+
+var array = ['valor1', 'valor2', 'valor3', 'valor4']
+for (let i = 0; i < array.length; i++){
+    console.log(i)
+}
+
+
+//Repetição - For/in
+
+var array = ['valor1', 'valor2', 'valor3', 'valor4']
+for (i in array){
+    console.log(i)
+}
+
+
+//Repetição - For/of
+var array = ['valor1', 'valor2', 'valor3', 'valor4']
+for (i of array){
+    console.log(i)
+}
+
+
+//Repetição - While
+var a = 0;
+while (a < 10){
+    a++
+    console.log(a)
+}
+
+
+//Repetição - Do/While
+var a = 0;
+do {
+    a++
+    console.log(a)
+}
+while (a < 10)
+*/
